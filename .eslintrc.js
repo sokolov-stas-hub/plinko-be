d98566f8de779f2e40274dfa7877cc384ba9701e
@stanislav.sokolov <stanish@deepvue.com>
2026-05-17 13:36:17 +0300

    chore: add minimal eslint and prettier configs

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  root: true,
+  env: { node: true, jest: true },
+  ignorePatterns: ['dist/', 'node_modules/', 'coverage/', '*.js'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+};
